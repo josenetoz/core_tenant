656414c7eadc42f577b3e17e31a1662031c5bf0a
wallace martins <wallacemartinss@gmail.com>
2025-01-20 10:02:08 -0300

    Atualizações gerais

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -14,7 +14,8 @@ class Dashboard extends BaseDashboard
     public function subscribeAction(): Action
     {
         return SubscribePlanAction::make()
-            ->brandLogo('https://wtasso.com.br/wp-content/uploads/2024/10/logo-deitado-geral-1024x348.png')
+            ->brandLogo('https://besttips.com.br/logo_bottom_part.png')
+            //->brandLogo('https://wtasso.com.br/wp-content/uploads/2024/10/logo-deitado-geral-1024x348.png')
             ->modalHeading(__(''))
             ->modalDescription(__('Selecione o Plano que deseja adquirir!'))
             ->modalWidth('2xl')
